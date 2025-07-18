1b90ec440a72c0919cfc120faf55f709641776be
kanata <kanata@mukuness.com>
2025-07-18 09:29:46 +0900

    HAPPY60のキーマップを登録

---
+++ keyboards/mukuness/happy60/config.h
@@ -0,0 +1,7 @@
+// Copyright 2025 Mukuness (@kanata)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+//#define BOOTMAGIC_LITE_ROW      0
+//#define BOOTMAGIC_LITE_COLUMN   1
---
+++ keyboards/mukuness/happy60/keymaps/default/keymap.c
@@ -0,0 +1,45 @@
+// Copyright 2025 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT_9x9(
+        // ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
+        // │ESC│ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ - │ = │ \ │ ` │
+        // ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┤
+        // │ TAB │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ [ │ ] │ BS  │
+        // ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤
+        // │ CTRL │ A │ S │ D │ F │ G │ H │ J │ K │ L │ ; │ ' │  ENTER │
+        // ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┤
+        // │ SHIFT  │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ / │SHIFT │FN │
+        // └─────┬──┴┬──┴──┬┴───┴───┴───┴───┴───┴───┼───┴─┬─┴─┬────┴───┘
+        //       │ALT│ CMD │                        │ CMD │ALT│
+        //       └───┴─────┴────────────────────────┴─────┴───┘
+        QK_BOOT,
+        KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSLS, KC_GRV ,
+        KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSPC,
+        KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,          KC_ENT ,
+        KC_LSFT,          KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, MO(1)  ,
+                          KC_LALT, KC_LGUI,                   KC_SPC ,                   KC_RGUI, KC_RALT
+    ),
+    [1] = LAYOUT_9x9(
+        // ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
+        // │PWR│F1 │F2 │F3 │F4 │F5 │F6 │F7 │F8 │F9 │F10│F11│F12│INS│DEL│
+        // ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┤
+        // │ TAB │VL-│VL+│MUT│ R │ T │ Y │ U │PSC│SCR│PSE│ ↑ │ ] │  \  │
+        // ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤
+        // │ CAPS │VA-│VA+│SP-│SP+│ G │ H │ J │HOM│PGU│ ← │ → │  ENTER │
+        // ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┤
+        // │ SHIFT  │TOG│MOD│HU-│HU+│ B │ N │ M │END│PGD│ ↓ │SHIFT │FN │
+        // └─────┬──┴┬──┴──┬┴───┴───┴───┴───┴───┴───┼───┴─┬─┴─┬────┴───┘
+        //       │ALT│ CMD │                        │ CMD │ALT│
+        //       └───┴─────┴────────────────────────┴─────┴───┘
+        XXXXXXX,
+        KC_PWR , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_INS, KC_DEL ,
+        _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, _______, KC_PSCR, KC_SCRL, KC_PAUS, KC_UP  , _______, _______,
+        KC_CAPS, RGB_VAD, RGB_VAI, RGB_SPD, RGB_SPI, _______, _______, _______, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT,          KC_PENT,
+        _______,          RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, _______, _______, _______, KC_END , KC_PGDN, KC_DOWN, _______, _______,
+                          _______, _______,                   _______,                   _______, _______
+    )
+};
